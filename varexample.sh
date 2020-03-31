de5314a1da2c041cffb904329604b4ce012d8f3f
rustamsariyev <r.sariyev@outlook.com>
2020-03-31 08:48:14 +0000

    Lecture: Setting and Using Variables in Scripts

---
+++ varexample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MYUSERNAME="username"
+MYPASSWORD="password"
+STARTOFSCRIPT=`date`
+
+echo "My login name for this application is: $MYUSERNAME"
+echo "My login password for this application is: $MYPASSWORD"
+echo "I started this script at: $STARTOFSCRIPT"
+
+ENDOFSCRIPT=`date`
+echo "I ended this script at: $ENDOFSCRIPT" 
