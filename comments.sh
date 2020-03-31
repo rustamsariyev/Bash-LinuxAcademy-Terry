e01627371793c2003d366e6411e0af8ec9f39fda
rustamsariyev <r.sariyev@outlook.com>
2020-03-31 09:16:16 +0000

    Lecture: Comment Types and Structure

---
+++ comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This line is intended to be used as a general description of the script and anything that it does
+
+clear  # Clears the screen
+
+# MYUSERNAME="Terry" # The usernamee for this application
+MYUSERNAME="Done"  # New username added later
+
+echo "We are using the default user called: $MYUSERNAME" # Display to the console
+
+DATETIMESTAMP=`date`
+
+echo "This is when the script was run: $DATETIMESTAMP" # This is the timestamp of run
+
