7c2cad93ba670f90b28955a57c803766dfec6c5a
rustamsariyev <r.sariyev@outlook.com>
2020-04-01 08:38:21 +0000

    Exercise: Display Environment Variables

---
+++ Exercises/display-environment-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+clear
+
+echo "This script will give us environment information"
+echo "================================================"
+echo ""
+echo "Hello Username:$USERNAME"
+echo ""
+echo "Your Home Directory is:$HOME"
+echo ""
+echo "Your Hostory File Will Ignore:$HISTCONTROL"
+echo ""
+echo "Your Terminal Session Type is:$TREM"
+echo ""
+
