5496225283c5e700b2fd1f7c582e52d30cf4cdd8
rustamsariyev <r.sariyev@outlook.com>
2020-04-01 08:18:59 +0000

    Lecture: Arrays

---
+++ arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Simple array list and loop for display
+
+SERVERLIST=("websrv01" "websrv02" "websrv03" "websrv04")
+COUNT=0
+
+for INDEX in ${SERVERLIST[@]}; do
+  echo "Processing Server: ${SERVERLIST[COUNT]}"
+  COUNT="`expr $COUNT + 1`"
+done
+
