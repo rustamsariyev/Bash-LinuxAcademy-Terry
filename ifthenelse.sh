8a10a04a53e73261757599eea618d1b506fd651e
rustamsariyev <r.sariyev@outlook.com>
2020-04-01 14:32:18 +0000

    Lecture: If/Then/ElseLecture: If/Then/Else

---
+++ ifthenelse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Simple example of if then else and nested if statements
+
+echo "Enter a number between 1 and 3: "
+read VALUE
+
+# OR condition
+# If  you don't want to see error messages using /dev/null  each condition
+#if [[ "$VALUE" -eq "1" ]] 2> /dev/null || [[ "$VALUE" -eq "2" ]] 2>/dev/null || [[ "$VALUE" -eq 3 ]] 2> /dev/null ; then
+
+# elif statements
+if [[ "$VALUE" -eq "1" ]] 2>/dev/null ; then
+  echo "You entered #1"
+
+elif [[ "$VALUE" -eq "2" ]] 2>/dev/null ; then
+  echo "You successfully entered #2"
+
+elif [[ "$VALUE" -eq "3" ]] 2>/dev/null ; then
+  echo "You entered the 3rd number" 
+else 
+  echo "You didn't follow directions!"
+fi
