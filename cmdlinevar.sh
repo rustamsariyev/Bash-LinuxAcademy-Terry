642cf94dc08a1e639e2407376210ada7c5c5ec70
rustamsariyev <r.sariyev@outlook.com>
2020-04-01 12:24:06 +0000

    Lecture: Passing Variables to Scripts at the Command Line

---
+++ cmdlinevar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Demo of command line values passed in with our shell script
+
+USERNAME=$1
+PASSWORD=$2
+
+echo "The following Username is $USERNAME and Password is $PASSWORD"
+
+
+#echo "The following item was passed in to script at run time $1 $2"
+
+
+
+
+
+
+
+# Execution history file: commands-cmdlinevars.txt
