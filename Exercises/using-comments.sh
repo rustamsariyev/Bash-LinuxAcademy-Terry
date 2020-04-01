834a910c70dab59c26a2e34ccc26d56c6d05d2cd
rustamsariyev <r.sariyev@outlook.com>
2020-04-01 08:51:02 +0000

    Exercise: Using Comments

---
+++ Exercises/using-comments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# This line is intended to be used as a general description of the script and anything that it does
+
+clear #clares the screen
+
+#MYUSERNAME="Terry" #the username for this application
+MYUSERNAME="Don" #new username addded later
+
+echo "We are using the default user called:$MYUSERNAME" #display to the console
+
+DATETIMESTAMP=`date`
+
+echo "This is when the script was run:$DATETIMESTAMP" #this is the timestamp of run
+
+
