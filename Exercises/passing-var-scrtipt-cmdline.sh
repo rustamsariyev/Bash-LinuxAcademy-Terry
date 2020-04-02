7a389ad211badc0b2443d6f4f37017faf08e6ceb
rustamsariyev <r.sariyev@outlook.com>
2020-04-02 11:46:25 +0000

    Exercise: Passing Variables to Scripts at the Command Line

---
+++ Exercises/passing-var-scrtipt-cmdline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Demo of command line values passed in with out shell script
+clear
+
+USERNAME=$1
+PASSWORD=$2
+
+echo "The following Username is $USERNAME and Password is $PASSWORD"
+
