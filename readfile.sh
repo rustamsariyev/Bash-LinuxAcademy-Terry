35629e61c3d4676ded56d615c3d0d1e1f57eeb51
rustamsariyev <r.sariyev@outlook.com>
2020-04-02 13:07:37 +0000

    Lecture: Reading Files

---
+++ readfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Simple file reading (non-binary) and displaying one line at a time
+
+echo "Enter a filename to read:"
+read FILE
+
+while read -r SUPERHERO; do
+  echo "Superhero Name: $SUPERHERO"
+done <$FILE
