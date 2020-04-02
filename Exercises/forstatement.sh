bb5533f4926a437bd5aab8ab95d769f91e5818f5
rustamsariyev <r.sariyev@outlook.com>
2020-04-02 12:03:01 +0000

    Exercise: The for Statement

---
+++ Exercises/forstatement.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This is a demo of the for loop
+
+echo "List all the shell scripts contents of the directory"
+
+SHELLSCRIPTS=`ls *.sh`
+
+for SCRIPT in $SHELLSCRIPTS ; do
+  DISPLAY="`cat $SCRIPT`"
+  echo "File:$SCRIPT -Contents $DISPLAY"
+done
+
