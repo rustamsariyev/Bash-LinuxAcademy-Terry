f6f91966fca102b6bc74063b2e7bf9a875f831e0
rustamsariyev <r.sariyev@outlook.com>
2020-04-02 15:52:00 +0000

    Execrcise:IFS and Delimiting

---
+++ Exercises/if-and-delimiting.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Delimiter examples using IFS
+
+echo "Enter filename to parse:"
+read FILE
+
+echo "Enter the Delimiter:"
+read DELIM
+
+IFS="$DELIM"
+
+while read -r CPU MEMORY DISK; do
+  echo "CPU: $CPU"
+  echo "Memory: $MEMORY"
+  echo "Disk: $DISK"
+done <"$FILE"
