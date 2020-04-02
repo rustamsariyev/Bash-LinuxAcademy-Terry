f16ef47a97cc46f8427b1b32cb5720b5fd9a9e07
rustamsariyev <r.sariyev@outlook.com>
2020-04-02 12:08:56 +0000

    Exercise: Using the Case Statement

---
+++ Exercises/casestatement.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Demo of the case statement
+
+clear
+
+echo "MAIN MENU"
+echo "=========="
+echo "1)Choice One"
+echo "2)Choice Two"
+echo "3)Choice Three"
+echo ""
+echo "Enter Choice:"
+read MENUCHOICE
+
+case $MENUCHOICE in 
+  1)
+    echo "Congratulations for Choosing the first  Option" ;;
+  2) 
+    echo "Choice 2 Chosen" ;;
+  3)
+    echo "Last Choice Made" ;;
+  *)
+    echo "You chose unwisely" ;;
+esac
