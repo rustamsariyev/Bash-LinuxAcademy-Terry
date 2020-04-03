a1f923100b086316cddb34de96ff79718df103f6
rustamsariyev <r.sariyev@outlook.com>
2020-04-03 10:15:37 +0000

    Lecture: Simple Functions

---
+++ simplefunc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This is a simple function example
+# : null 
+# ; end of line
+
+echo "Starting the function definition..."
+
+funcExample () {
+echo "We are now INSIDE the function..."
+}
+
+funcExample
